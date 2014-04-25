c0ac9a09c67a77f4f4daee911aaed41053b3d9b9
Alumne <Alumne@192.168.17.47>
2014-04-25 11:03:55 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -34,10 +34,13 @@ public class Prjava01 {
             bw.newLine();
             bw.write("Novap&agrave;ginaweb");
             bw.newLine();
+            bw.write("<h1>DAW2m08uf4pr2</h1>");
+            bw.newLine();
             bw.write("</body>");
             bw.newLine();
             bw.write("</html>");
             bw.newLine();
+
             bw.close();
         }
     }
